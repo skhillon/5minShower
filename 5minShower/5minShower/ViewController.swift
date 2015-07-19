abd808b3c743cd280c4c00f1392bc8d2efbbf6ff
Laikh Tewari <laikhtewari1@gmail.com>
2015-07-18 20:50:29 -0700

    added stop button, added animation for buttons, lookin dope

--- 5minShower/5minShower/ViewController.swift
+++ 5minShower/5minShower/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.stopButton.hidden = true
+        self.stopButton.alpha = 0
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -21,15 +23,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    
     let myMusicPlayer = MPMusicPlayerController()
     let myMediaQuery = MPMediaQuery()
     
     @IBAction func playButtonTapped(sender: AnyObject) {
+        println ("play button tapped")
         myMusicPlayer.setQueueWithQuery(myMediaQuery)
         myMusicPlayer.play()
+        UIView.animateWithDuration(2.0, animations: {
+            self.playButton.alpha = 0
+            self.stopButton.alpha = 1
+            self.stopButton.hidden = false
+        })
+        self.playButton.hidden = true
     }
     
-    @IBAction func pauseButtonTapped(sender: AnyObject) {
+    @IBAction func stopButtonTapped(sender: AnyObject) {
         stop()
     }
     
@@ -37,4 +50,5 @@ class ViewController: UIViewController {
     {
         myMusicPlayer.pause()
     }
+    
 }
